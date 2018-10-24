c89ce9c1df4ee6246d56f002756646cbbd729737
Dave Mateer <davemateer@gmail.com>
2018-10-24 12:57:33 +0100

    auto commit

--- OpenVSSolution/Program.cs
+++ OpenVSSolution/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace OpenVSSolution
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // cd C:\dev\test\OpenVSSolution
+            // dotnet publish -c Release -r win10-x64
+            // open this foldere: C:\dev\test\OpenVSSolution\OpenVSSolution\bin\Release\netcoreapp2.1\win10 - x64
+            // copy the entire directory C:\sharedTools\OpenVSSolution
+
+            var currentPath = Directory.GetCurrentDirectory();
+            var files = new DirectoryInfo(currentPath).GetFiles().Where(x => x.Extension == ".sln");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"{file.Name,-20} : Opening this file! "); // nice console formatting
+                var proc = new Process();
+                var devenvpath = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\";
+                proc.StartInfo.FileName = devenvpath + "devenv";
+                proc.StartInfo.Arguments = currentPath + @"\" + file.Name;
+                proc.Start();
+            }
         }
     }
 }
