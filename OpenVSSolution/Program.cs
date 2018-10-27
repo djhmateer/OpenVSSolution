8ca3f1be8ce60e7a24bf93d25b4fdf44e706f980
Dave Mateer <davemateer@gmail.com>
2018-10-27 07:42:10 +0100

    auto commit

--- OpenVSSolution/Program.cs
+++ OpenVSSolution/Program.cs
@@ -14,14 +14,28 @@ namespace OpenVSSolution
             // open this foldere: C:\dev\test\OpenVSSolution\OpenVSSolution\bin\Release\netcoreapp2.1\win10 - x64
             // copy the entire directory C:\sharedTools\OpenVSSolution
 
+            // https://stackoverflow.com/a/48897649/26086 - publishing with dependencies. not sure if works
+            //   An assembly specified in the application dependencies manifest (d.deps.json) was not found:
+            // package: 'runtime.win-x64.Microsoft.NETCore.App', version: '2.1.5'
+            // path: 'runtimes/win-x64/lib/netcoreapp2.1/Microsoft.CSharp.dll'
+
             var currentPath = Directory.GetCurrentDirectory();
             var files = new DirectoryInfo(currentPath).GetFiles().Where(x => x.Extension == ".sln");
+
+            // where is VS - Community or Enterprise?
+            var devenv = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\";
+            var vsversion = new DirectoryInfo(devenv).GetDirectories().Where(x => x.Name == "Community");
+
+            var devenvpath = devenv; 
+            if (vsversion.Count() == 1)
+                devenvpath += @"Community\Common7\IDE\";
+            else
+                devenvpath += @"Enterprise\Common7\IDE\";
 
             foreach (var file in files)
             {
                 Console.WriteLine($"{file.Name,-20} : Opening this file! "); // nice console formatting
                 var proc = new Process();
-                var devenvpath = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\";
                 proc.StartInfo.FileName = devenvpath + "devenv";
                 proc.StartInfo.Arguments = currentPath + @"\" + file.Name;
                 proc.Start();
