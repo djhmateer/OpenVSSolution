9f667d87d872acd73388d44ad5814d15bbd7e56d
Dave Mateer <davemateer@gmail.com>
2019-04-04 10:46:03 +0100

    Using VS2019 and fallback to 2017

--- OpenVSSolution/Program.cs
+++ OpenVSSolution/Program.cs
@@ -24,13 +24,25 @@ namespace OpenVSSolution
                 return;
             }
 
-            // Where is VS - Community or Enterprise?
-            var devenvpath = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\";
-            var vsdiretory = new DirectoryInfo(devenvpath).GetDirectories();
+            // Prefer VS2019 then downgrade to VS2017 if not there
+            //var devenvpath = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\";
+            var devenvpath = @"C:\Program Files (x86)\Microsoft Visual Studio\";
+            var vsDirectoryVersion = new DirectoryInfo(devenvpath).GetDirectories();
+            if (vsDirectoryVersion.Any(x => x.Name == "2019"))
+                devenvpath += @"2019\";
+            else if (vsDirectoryVersion.Any(x => x.Name == "2017"))
+                devenvpath += @"2017\";
+            else
+            {
+                Console.WriteLine($"Neither Visual Studio Community nor Enterprise can be found in {devenvpath}");
+                return;
+            }
 
-            if (vsdiretory.Any(x => x.Name == "Community"))
+            var vsDirectory = new DirectoryInfo(devenvpath).GetDirectories();
+            // Where is VS - Community or Enterprise?
+            if (vsDirectory.Any(x => x.Name == "Community"))
                 devenvpath += @"Community\Common7\IDE\";
-            else if (vsdiretory.Any(x => x.Name == "Enterprise"))
+            else if (vsDirectory.Any(x => x.Name == "Enterprise"))
                 devenvpath += @"Enterprise\Common7\IDE\";
             else
             {
@@ -39,7 +51,7 @@ namespace OpenVSSolution
             }
 
             // Call VS in a new process and return to the shell
-            Console.WriteLine($"{slnfile.Name,-20} : Opening this file! "); 
+            Console.WriteLine($"{slnfile.Name,-20} : Opening this file! ");
             var proc = new Process();
             proc.StartInfo.FileName = devenvpath + "devenv";
             proc.StartInfo.Arguments = currentPath + @"\" + slnfile.Name;
